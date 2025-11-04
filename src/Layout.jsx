d2f5956152a69bfec8465fbb3068601aea1a2599
m.saher <mohsaher9900@gmail.com>
2025-11-04 15:51:45 +0200

    testing the footer route

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./shared/NaveBar/NaveBar";
+import Footer from './shared/Footer/Footer'
 
 function Layout() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 }
---
+++ src/shared/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.style.css";
+
+function Footer() {
+  return (
+  <div className="Footer">Welcome To The Footer</div>
+  )
+}
+
+export default Footer;
